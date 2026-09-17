a0f4d2965e6d36818530b895349ccf6d35e1d006
leslie123-bee <fanyilson5@gmail.com>
2026-09-17 03:55:05 +0000

    Add password update route to users_routes

--- backend/src/routes/users_routes.py
+++ backend/src/routes/users_routes.py
@@ -1,4 +1,4 @@
-from ..controllers.users_controller import get_me, update_me
+from ..controllers.users_controller import get_me, update_me, update_password
 from ..lib.http_kit import create_router
 from ..middleware.auth_middleware import authenticate
 
@@ -6,3 +6,4 @@ router = create_router()
 router.use(authenticate)
 router.get('/me', get_me)
 router.patch('/me', update_me)
+router.patch('/me/password', update_password)
